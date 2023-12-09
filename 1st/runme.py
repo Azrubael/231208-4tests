89c808db3def50110bd5e35723881f4ca67f43bd
Azrubael <i0638464000@gmail.com>
2023-12-09 11:16:17 +0200

    #6 1st task uptated with handling exceptions.

--- 1st/runme.py
+++ 1st/runme.py
@@ -1,15 +1,23 @@
 import json
 
 
-def read_json(file_name):
-    with open(file_name, 'r') as f:
-        return json.load(f)
+def read_json(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            return json.load(f)
+    except:
+        print(f'File {file_path} reading error.')
 
 
-def save_json(data, file_name):
-    with open(file_name, 'a') as f:
-        json.dump(data, f, indent=4)
-        f.write('\n')
+
+def save_json(data, file_path):
+    try:
+        with open(file_path, 'a') as f:
+            json.dump(data, f, indent=4)
+            f.write('\n')
+    except:
+        print(f'File {file_path} writing error.')
+
 
 
 in_data = read_json('1st/input.json')
@@ -19,10 +27,11 @@ factor = rules[in_data['distance']['unit']][in_data['convert_to']]
 
 the_result = {'unit':'','value':''}
 the_result['unit'] = in_data['convert_to']
-the_result['value'] = round( in_length * factor, 3)
 
-print(the_result)
+try:
+    the_result['value'] = round( in_length * factor, 3)
+except:
+    print(f'Calculation error during multipy {in_length} and {factor}')
+
+print(f'Output result: {the_result}')
 save_json(the_result, '1st/output.json')
-
-
-
